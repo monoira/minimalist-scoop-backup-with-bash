cc98a08208acc2a138ee1b622e0d6cf2680d759d
Irakli Tatarashvili <116330974+monoira@users.noreply.github.com>
2024-06-12 10:04:31 +0400

    Update backup-scoop.sh

--- backup-scoop.sh
+++ backup-scoop.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Define the output JSON file path
 output_file="exported.json"
 
-# Run the scoop export command and store the output in the JSON file
+# runs the scoop export command and store the output in the JSON file
 # if there is already content inside output_file, content will be overwritten.
 scoop export >"$output_file"
 
-# Check if the scoop export command was successful
+# Checking if the scoop export command was successful
 if [ $? -eq 0 ]; then
     echo "Export completed successfully. JSON file saved as $output_file"
 else
